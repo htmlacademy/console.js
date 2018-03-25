429ee283352edfd820d49124b6c767377c2a6473
Николай Эмрих <redishko@gmail.com>
2018-03-25 22:04:00 +0300

    Edited config

--- js/index.js
+++ js/index.js
@@ -3,11 +3,13 @@ import {ViewType} from "./enums";
 
 window.jsConsole = new Console(document.querySelector(`.console`), {
   object: {
-    maxFieldsInHead: 2
+    maxFieldsInHead: 2,
+    expandDepth: 2,
+    exclude: [ViewType.ARRAY]
   },
   // function: {
-  //   expandDepth: 2,
-  //   exclude: [ViewType.OBJECT, ViewType.ARRAY]
+    // expandDepth: 2,
+    // exclude: [ViewType.OBJECT, ViewType.ARRAY]
   // },
   // array: {
   //   expandDepth: 2,
