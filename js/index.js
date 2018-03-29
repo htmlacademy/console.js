8149399a5e73e51229ac2f613b7520beea523f39
Николай Эмрих <redishko@gmail.com>
2018-03-29 09:27:30 +0300

    Fix console init

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
 import Console from './main';
-import {ViewType} from "./enums";
 
-window.jsConsole = new Console(document.querySelector(`.console`), {
-  object: {
-    maxFieldsInHead: 2,
-    expandDepth: 2,
-    exclude: [ViewType.ARRAY]
-  },
-  // function: {
-  //   expandDepth: 2,
-  //   exclude: [ViewType.OBJECT, ViewType.ARRAY]
-  // },
-  // array: {
-  //   expandDepth: 2,
-  //   exclude: [ViewType.OBJECT, ViewType.FUNCTION]
-  // }
-});
+window.Console = Console;
