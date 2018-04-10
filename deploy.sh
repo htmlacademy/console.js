9c569af3dbe5de2b8acb93412f16141422f9c00e
Николай Эмрих <redishko@gmail.com>
2018-04-10 18:15:18 +0300

    Add new deploy method including semver

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]') &&
+curl -LOk https://github.com/htmlacademy/console.js/archive/gh-pages.zip &&
+unzip ./gh-pages.zip &&
+rm -rf ./console.js-gh-pages/$PACKAGE_VERSION &&
+cp -r ./build ./console.js-gh-pages/$PACKAGE_VERSION &&
+rm -rf ./console.js-gh-pages/latest &&
+cp -r ./build ./console.js-gh-pages/latest &&
+gh-pages -d ./console.js-gh-pages/ &&
+rm -rf ./console.js-gh-pages ^^
+rm -rf ./gh-pages.zip
