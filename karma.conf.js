8efa96f6ca227035c85ee4e9847f1767fa46d226
Николай Эмрих <redishko@gmail.com>
2018-04-28 14:47:31 +0300

    test

--- karma.conf.js
+++ karma.conf.js
@@ -10,18 +10,18 @@ module.exports = function (config) {
       format: `%b %T: %m`,
       terminal: true
     },
-    singleRun: false,
+    // singleRun: false,
     client: {
       mocha: {
         timeout: 15000 // 6 seconds - upped from 2 seconds
       }
     },
-    browsers: [`Chrome`],
-    // customLaunchers: {
-    //   ChromeHeadlessNoSandbox: {
-    //     base: `ChromeHeadless`,
-    //     flags: [`--no-sandbox`]
-    //   }
-    // }
+    browsers: [`ChromeHeadless`],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: `ChromeHeadless`,
+        flags: [`--no-sandbox`]
+      }
+    }
   });
 };
