cb47f666948bc9e1bfd397f2f33a1b407c0100e0
nikolayemrikh <nikolayemrikh@htmlacademy.ru>
2018-11-13 23:25:07 +0300

    Updates

---
+++ 0.4/js/presets/htmlacademy.js
@@ -0,0 +1,1 @@
+!function(){"use strict";var o={function:{nowrapOnLog:!0},common:{excludePropertiesFromAutoexpand:["__proto__"],maxFieldsInHead:5,minFieldsToAutoexpand:5,maxFieldsToAutoexpand:15}};Array.isArray(window.jsConsolePresets)?window.jsConsolePresets.push(o):window.jsConsolePresets=[o]}();
