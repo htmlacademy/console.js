2a7a3943c52ae1fb36aebacc58ebcd7b238a790c
nikolayemrikh <nikolayemrikh@htmlacademy.ru>
2018-11-19 22:31:24 +0300

    Updates

---
+++ 1.0.0/js/presets/htmlacademy.js
@@ -0,0 +1,1 @@
+!function(){"use strict";window.jsConsole.addConfig({function:{nowrapOnLog:!0},common:{excludePropertiesFromAutoexpand:["__proto__"],maxFieldsInHead:5,minFieldsToAutoexpand:5,maxFieldsToAutoexpand:15}})}();
