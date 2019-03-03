883a1e5173e564475d3510444278b162a8aac999
nikolayemrikh <nikolayemrikh@htmlacademy.ru>
2019-03-03 03:07:42 +0300

    Чинит

--- src/node/node-view.js
+++ src/node/node-view.js
@@ -7,16 +7,14 @@ const ELS_WITHOUT_ENDING_TAG = [`input`, `br`, `hr`, `img`, `base`, `link`, `met
 
 const MAX_PREVIEW_CONTENT_LENGTH = 43;
 
-const ContentType = {
-  PLAIN: `plain`,
-  NODES: `nodes`
-};
-
 const getStateDescriptorsKey = Symbol(GET_STATE_DESCRIPTORS_KEY_NAME);
 
 const getElementAttributesStr = (el) => (
   Array.from(el.attributes)
-    .map(({name, value}) => `<span class="c-html__attr-name">${name}=</span>&quot;<span class="c-html__attr-value">${value}</span>&quot;`)
+    .map(({name, value}) => (
+      `<span class="c-html__attr-name">${name}=</span>\
+&quot;<span class="c-html__attr-value">${value}</span>&quot;`
+    ))
     .join(` `)
 );
 
@@ -36,6 +34,7 @@ export default class NodeView extends BaseView {
     }
 
     this._stateDescriptorsQueue.push(this[getStateDescriptorsKey]());
+    this._closingTag = this._getClosingTag();
   }
 
   get template() {
@@ -99,12 +98,8 @@ export default class NodeView extends BaseView {
       return true;
     }
 
-    if (this._mode === Mode.PROP) {
-      return false;
-    }
-
     return !(
-      this._value.nodeType === Node.ELEMENT_NODE ||
+      (this._value.nodeType === Node.ELEMENT_NODE && this._closingTag) ||
       this._value.nodeType === Node.DOCUMENT_FRAGMENT_NODE ||
       this._value.nodeType === Node.DOCUMENT_NODE
     );
@@ -136,7 +131,7 @@ export default class NodeView extends BaseView {
     } else if (v.nodeType === Node.DOCUMENT_NODE) {
       val = `#document`;
     } else if (v.nodeType === Node.COMMENT_NODE) {
-      val = `<span class="c-html__comment">&lt;!-- ${v.data} --&gt;</span>`;
+      val = `<span class="c-html__comment">&lt;!--${v.data}--&gt;</span>`;
     } else if (v.nodeType === Node.ATTRIBUTE_NODE) {
       val = `<span class="c-html__attr-name">${v.name}</span>`;
     } else if (v.nodeType === Node.DOCUMENT_TYPE_NODE) {
@@ -182,7 +177,7 @@ export default class NodeView extends BaseView {
         }
       }
 
-      let closingTag = this._getClosingTag();
+      let closingTag = this._closingTag;
       return `${content}${closingTag}`;
     } else if (this._value.nodeType === Node.ATTRIBUTE_NODE) {
       const attrValue = this._value.value;
@@ -192,17 +187,13 @@ export default class NodeView extends BaseView {
   }
 
   _getClosingTag() {
-    const tagNameLower = this._value.tagName.toLowerCase();
-    if (!ELS_WITHOUT_ENDING_TAG.includes(tagNameLower)) {
-      return `<span class="c-html__tag-name">&lt;/${tagNameLower}&gt;</span>`;
+    if (this._value.nodeType === Node.ELEMENT_NODE) {
+      const tagNameLower = this._value.tagName.toLowerCase();
+      if (!ELS_WITHOUT_ENDING_TAG.includes(tagNameLower)) {
+        return `<span class="c-html__tag-name">&lt;/${tagNameLower}&gt;</span>`;
+      }
     }
     return ``;
-  }
-
-  _getContentType() {
-    return (
-      this._mode === Mode.PREVIEW
-    ) ? ContentType.PLAIN : ContentType.NODES;
   }
 
   createContent(node) {
@@ -210,7 +201,7 @@ export default class NodeView extends BaseView {
 
     const mode = Mode.PROP;
 
-    if (node.nodeType === Node.ELEMENT_NODE) {
+    if (node.nodeType === Node.ELEMENT_NODE || node.nodeType === Node.DOCUMENT_FRAGMENT_NODE || node.nodeType === Node.DOCUMENT_NODE) {
       if (this._console.checkInstanceOf(node, `HTMLTemplateElement`)) {
         const val = node.content;
         const nodeView = new NodeView({val, mode, depth: this.nextNestingLevel, parentView: this}, this._console);
@@ -227,15 +218,12 @@ export default class NodeView extends BaseView {
       }
     }
 
-    const closingTag = this._getClosingTag();
+    const closingTag = this._closingTag;
     if (closingTag) {
       const entryEl = getElement(closingTag);
       const el = new EntryView({entryEl, withoutKey: true}).el;
       fragment.appendChild(el);
     }
-
-    // const keyEl = this._console.createTypedView(this._pairKey, this._mode, this.nextNestingLevel, this, this._propKey).el;
-
 
     return {fragment};
   }
