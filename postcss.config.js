e986a9d501737647718be42484196e1bdf5562c1
Sergey Kluchkovsky <kaineer@gmail.com>
2022-07-26 16:45:56 +0500

    Добавляет опции для postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,20 @@
 // postcss.config.js
-// TODO: добавить browserlist
-// TODO: добавить mqpacker
+
+const overrideBrowserslist = [
+  "last 1 version",
+  "last 2 Chrome versions",
+  "last 2 Firefox versions",
+  "last 2 Opera versions",
+  "last 2 Edge versions"
+];
+
+module.exports = {
+  plugins: [
+    require("autoprefixer")({
+      overrideBrowserslist
+    }),
+    require("css-mqpacker")({
+      sort: true
+    })
+  ]
+};
