862d145a15b284481ac715a88209f395c06b546e
Sergey Kluchkovsky <kaineer@gmail.com>
2022-07-27 13:18:59 +0500

    Добавляет сборку пресетов

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,18 @@
 // TODO: добавить возможность строить в процессе разработки
 // TODO: а потом можно будет вычистить gulp совсем
 
+// ## Artefacts to build
+//
+//  * [x] css/style.css, css/style.min.css
+//  * [x] css/prism.css, css/prism.min.css
+//  * [x] js/index{,-silent,-prompt}.js
+//  * [ ] js/presets ?
+//  * [ ] js/tests
+
 const { join } = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { sync: glob } = require("glob");
 
 const env = () => (
   process.env.NODE_ENV === "production" ?
@@ -43,7 +52,7 @@ const getPostcssOptions = (minify) => {
   };
 }
 
-const jsEntries = () => [
+const jsIndexEntries = () => [
   "index",           // window.Console
   "index-prompt",    // window.Prompt
   "index-silent"     // hell if i know
@@ -51,6 +60,19 @@ const jsEntries = () => [
     (entries, name) => ({...entries, ["js/" + name]: "./src/" + name + ".js"}),
     {}
   );
+
+const jsPresetEntries = () => {
+  const files = glob("./src/presets/*.js");
+  return files.reduce((entries, file) => {
+    const id = "js/" + file.slice("./src/".length);
+    return {...entries, [id]: file};
+  }, {});
+};
+
+const jsEntries = () => ({
+  ...jsPresetEntries(),
+  ...jsIndexEntries(),
+});
 
 const fixWithMinify = (minify) => (
   minify ? (id) => id + ".min" : (id) => id
